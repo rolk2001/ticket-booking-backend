5f2fc60150d6eae6b2c5f3e8c8d4ba1a044d13cc
rolk2001 <tadmbayero@gmail.com>
2025-06-23 08:48:37 +0100

    Ajout de l'attribut photo au model Users

--- models/User.js
+++ models/User.js
@@ -6,7 +6,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   mot_de_passe: { type: String, required: true },
   type: { type: String, enum: ['client', 'admin'], default: 'client' },
-  telephone: { type: String, required: true }
+  telephone: { type: String, required: true },
+  photo: { type: String } // base64 ou URL
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
