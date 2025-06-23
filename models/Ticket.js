2a6745dd278b4a7b3cb2dcbf7907589c336eb997
rolk2001 <tadmbayero@gmail.com>
2025-06-23 11:50:28 +0100

    modification de schedule_id par schedule

--- models/Ticket.js
+++ models/Ticket.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 const ticketSchema = new mongoose.Schema({
   reservation_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Reservation', required: true },
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  schedule_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Schedule', required: true },
-  qr_code: { type: String, required: true }, // Contient l'image du QR code en format Data URL
-  seat: { type: String, default: 'Non assigné' } // On gérera la sélection de siège plus tard
+  schedule: { type: mongoose.Schema.Types.ObjectId, ref: 'Schedule', required: true }, // <-- corrige ici
+  qr_code: { type: String }, // Peut être optionnel si pas toujours généré à la création
+  seat: { type: String, default: 'Non assigné' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Ticket', ticketSchema);
