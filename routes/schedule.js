184b9552549d880be5194eb6216950563469c2aa
rolk2001 <tadmbayero@gmail.com>
2025-06-23 10:10:16 +0100

    Ajout nouvelle route pour seat

--- routes/schedule.js
+++ routes/schedule.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const scheduleController = require('../controllers/scheduleController');
+const ticketController = require('../controllers/ticketController');
 
 router.post('/', scheduleController.creerHoraire);
 router.get('/', scheduleController.listerHoraires);
+router.get('/:scheduleId/seats', ticketController.getReservedSeats);
 
 module.exports = router;
