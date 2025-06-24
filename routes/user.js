a949176e5023ef5fcbda75415379439923ad4ad9
rolk2001 <tadmbayero@gmail.com>
2025-06-24 17:10:09 +0100

    nouvelle route pour user

---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
+const userController = require('../controllers/userController');
+
+// Protéger la route avec le middleware d'authentification
+router.get('/messages', authMiddleware, userController.getMyMessages);
+
+module.exports = router; 
