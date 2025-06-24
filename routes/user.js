5cf96c8cc15ad7ac1966b49c9efcf45cbc41ec68
rolk2001 <tadmbayero@gmail.com>
2025-06-24 21:59:09 +0100

    Ajout de la route pour suppression message

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,7 @@ const userController = require('../controllers/userController');
 // Protéger la route avec le middleware d'authentification
 router.get('/messages', authMiddleware, userController.getMyMessages);
 
+router.delete('/messages/:id', authMiddleware, userController.deleteMessage);
+
+
 module.exports = router; 
